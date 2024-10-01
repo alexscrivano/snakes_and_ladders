2aee1fa5e564457cab7d7f5f1037dcb940f770d1
alexscrivano <scrlsn02m07d086t@studenti.unical.it>
2024-10-01 18:35:09 +0200

    Some updates

--- src/main/java/org/example/game/GameManager.java
+++ src/main/java/org/example/game/GameManager.java
@@ -25,7 +25,7 @@ public class GameManager {
             GameType gameType,
             Map<Player,PlayerTurnState> gturns
     )implements Serializable {}
-    private final String path = System.getProperty("user.dir") + "\\src\\main\\resources\\savings";
+    private final String path = ".\\Documents\\";
 
 
     private Application app;
